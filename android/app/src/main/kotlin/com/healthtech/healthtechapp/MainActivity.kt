2788de44c8578b8c9512cec2ce222b73711932b9
BrunoLeonardoRamosRibeiro <65317202+BrunoLeonardoRamosRibeiro@users.noreply.github.com>
2020-12-12 22:56:30 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/healthtech/healthtechapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.healthtech.healthtechapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Health Tech',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
+
